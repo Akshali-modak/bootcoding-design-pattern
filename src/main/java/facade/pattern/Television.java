ab02ecf18e71f6f27cbb25fa4365e973383d57da
bootcoding#123 <ModakAkshali211@gmail.com>
2023-08-23 13:49:17 +0530

    Singleton design pattern practice

---
+++ src/main/java/facade/pattern/Television.java
@@ -0,0 +1,11 @@
+package facade.pattern;
+
+public class Television {
+    public void switchOnTV(){
+        System.out.println("TV ON");
+    }
+
+    public void switchOfTV(){
+        System.out.println("TV OFF");
+    }
+}
